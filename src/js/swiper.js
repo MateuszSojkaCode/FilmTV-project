82bc95ab0abadfc54f1c21ce4e4dde7934c302d9
Mateusz <matsoj01@gmail.com>
2023-02-24 16:25:54 +0000

    working on slider

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,22 +1,26 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2.5,
-    slidesPerGroup: 2,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      mousewheel: true,
-      keyboard: true,
-    // breakpoints: {
-    //     769: {
-    //       slidesPerView: 2,
-    //       slidesPerGroup: 2,
-    //     },
-    //   },
-  });
+const swiper = new Swiper(".mySwiper", {
+	slidesPerView: 2.5,
+	slidesPerGroup: 2,
+	spaceBetween: 20,
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	mousewheel: true,
+	keyboard: true,
+	breakpoints: {
+		576: {
+			slidesPerView: 3.5,
+			slidesPerGroup: 3,
+		},
+		768: {
+			slidesPerView: 4.5,
+			slidesPerGroup: 4,
+		},
+	},
+});
