6ea25e0f93f0a0854e41edf8a839806b76bfd265
Mateusz <matsoj01@gmail.com>
2023-03-06 15:55:44 +0000

    section sport is finished

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -28,3 +28,35 @@ const swiper = new Swiper(".mySwiper", {
 		},
 	},
 });
+
+
+const swiperSport = new Swiper(".mySwiper__sport", {
+	slidesPerView: 1.5,
+	slidesPerGroup: 1,
+	spaceBetween: 20,
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	mousewheel: true,
+	keyboard: true,
+	breakpoints: {
+		576: {
+			slidesPerView: 2.5,
+			slidesPerGroup: 2,
+		},
+		768: {
+			slidesPerView: 3.5,
+			slidesPerGroup: 3,
+		},
+		992: {
+			slidesPerView: 3.5,
+			slidesPerGroup: 3,
+		},
+	},
+});
